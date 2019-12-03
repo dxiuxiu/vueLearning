c8915b8ee88bb196a32a7afca9c57f6d9e7d4b54
duxiuxiu <duxx@summit.com.cn>
2019-12-03 14:41:38 +0800

    vue-router 基础

---
+++ codes/2-vue-router-base/src/pages/b.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="" id="">
+        page B
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   activated() {
+   }
+}
+</script>
+
+<style scoped>
+/* css */
+</style>
