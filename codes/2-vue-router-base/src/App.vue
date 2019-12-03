26145bbfac224edc51baf77c96df24c7184f6fad
duxiuxiu <duxx@summit.com.cn>
2019-12-03 13:34:28 +0800

    feat(2-vue-router-base):vue 路由基础示例

---
+++ codes/2-vue-router-base/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Hello World!"/> -->
+    <HelloWorld msg='<span style = "color:red">Hello World!</span>'/>
+    <ev/>
+    <!-- <com age = "19"/> -->
+
+    <!-- 向子组件传递数据 -->
+    <com :age = "parentAge" @patch = "msg">
+      <header slot ="header">头部</header>
+      <footer slot ="footer">尾部</footer>
+    </com>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import './components/n'
+import ev from './components/events'
+import com from './components/com'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    ev,
+    com
+  },
+  data () {
+    return {
+      parentAge : 19
+    }
+  },
+  methods: {
+    msg:function(p){
+      this.parentAge++
+      window.console.log(p)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+header,footer{
+  background-color: #407ceb;
+  /* height: 30px; */
+}
+</style>
