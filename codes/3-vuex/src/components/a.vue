40604b4ddee85ba48af7dfa51895af4e6c0937ea
duxiuxiu <duxx@summit.com.cn>
2019-12-28 17:36:08 +0800

    vuex 处理异步

--- codes/3-vuex/src/components/a.vue
+++ codes/3-vuex/src/components/a.vue
@@ -3,13 +3,14 @@
         pageA {{$store.state.money.money}}
 
         <button @click = "add">money 增加</button>
+        <button @click = "asyncAdd">money 异步增加</button>
         <button @click = "reduce">money 减少</button>
     </div>
 </template>
 <script>
 import {mapActions} from 'vuex'
 export default {
-    methods: mapActions('money',['add','reduce']),
+    methods: mapActions('money',['add','reduce','asyncAdd']),
    data() {
       return {
       }
--- codes/3-vuex/src/store/modules/a.js
+++ codes/3-vuex/src/store/modules/a.js
@@ -1,16 +1,23 @@
 
 const state = {
-    money:0
+    money:0,
+    loading:false
 }
 
 const mutations = {
     add(state){
+        console.log(state)
         state.money++
     },
 
     reduce(state){
         state.money--
-    }
+    },
+    toggleLoading(state){
+        const loading = state.loading
+        state.loading = !loading
+        console.log(state.loading)
+    },
 }
 
 /** actions 中通过 commit 触发 mutations 中的数据*/
@@ -20,6 +27,19 @@ const actions = {
     },
     reduce:({commit}) =>{
         commit('reduce')
+    },
+    asyncAdd({ commit }){
+        commit('toggleLoading')
+        setTimeout(function(){
+            /** 提交 mutation 是更改状态的唯一方法，并且这个过程是同步的。 */
+            commit('add')
+            commit('toggleLoading')
+        },1000)
+
+        // commit('add')
+    },
+    toggleLoading({commit}){
+        commit('toggleLoading')
     }
 }
 
